4e620caf9cdff11e6fab19a83850a078bd14e6e6
Jozef Nagy <schkwve@gmail.com>
2023-09-09 11:33:42 +0200

    Added license header

--- krnl/arch/x86_64/dd/apic/pic.c
+++ krnl/arch/x86_64/dd/apic/pic.c
@@ -1,3 +1,13 @@
+/**
+ * LuxeOS (c) 2023 by Jozef Nagy
+ *
+ * LuxeOS is licensed under a
+ * Creative Commons Attribution-NoDerivatives 4.0 International License.
+ *
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-nd/4.0/>.
+ */
+
 #include <dd/apic/pic.h>
 
 #include <luxe.h>
